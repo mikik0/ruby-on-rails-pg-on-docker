86cb05d7f7ff3681575c8ac32b88dd3e37c8b60d
mikik0 <usapon_love1222@yahoo.co.jp>
2022-01-04 15:25:28 +0900

    add create

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -10,9 +10,31 @@ class TasksController < ApplicationController
         render json: @task
     end
 
+    def create
+        @task = Task.new()
+        title = task_params["title"]
+        content = task_params["content"]
+        @task.title = title
+        @task.content = content
+        p @task
+        if @task.save
+            render json: @task, status: :created, location: @task
+        else
+            render json: @task.errors, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @task.destroy
+    end
+
     private
 
     def set_task
         @task = Task.find(params[:id])
+    end
+
+    def task_params
+        params.require(:task).permit(:title, :content)
     end
 end
