cb05159a0d1667a107a32e552d347c38dd16b558
mikik0 <usapon_love1222@yahoo.co.jp>
2022-02-08 19:48:21 +0900

    終了期限のソートのテスト

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -47,6 +47,6 @@ class TasksController < ApplicationController
     end
 
     def task_params
-        params.require(:task).permit(:title, :content, :finished_at)
+        params.require(:task).permit(:title, :content, :deadline)
     end
 end
--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -1,14 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe "Tasks", type: :request do
-  let!(:task1) { create(:task, id: 1, finished_at: DateTime.new(2022, 1, 25)) }
-  let!(:task2) { create(:task, id: 2, finished_at: DateTime.new(2022, 1, 27)) }
-  let!(:task3) { create(:task, id: 3, finished_at: DateTime.new(2022, 1, 26)) }
+  let!(:task1) { create(:task, id: 1, created_at: DateTime.new(2022, 1, 14), deadline: DateTime.new(2022, 1, 25)) }
+  let!(:task2) { create(:task, id: 2, created_at: DateTime.new(2022, 1, 13), deadline: DateTime.new(2022, 1, 27)) }
+  let!(:task3) { create(:task, id: 3, created_at: DateTime.new(2022, 1, 12), deadline: DateTime.new(2022, 1, 26)) }
   
   # Taskが降順に並んでいるか確認
   describe "GET /tasks" do
-    it "Taskが終了期限に並んでいるか確認" do
+    it "Taskが降順に並んでいるか確認" do
       get tasks_path
+      expect(response.body).to eq([task1, task2, task3].to_json)
+    end
+  end
+  describe "GET /tasks?deadline" do
+    it "Taskが終了期限に並んでいるか確認" do
+      get tasks_path(sort: "deadline")
       expect(response.body).to eq([task2, task3, task1].to_json)
     end
   end
