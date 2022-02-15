3fe0b01dce477abcdbd23fb6ed0f756bfed229ea
mikik0 <usapon_love1222@yahoo.co.jp>
2022-02-15 18:44:22 +0900

    ページネーション

--- app/controllers/concerns/pagination.rb
+++ app/controllers/concerns/pagination.rb
@@ -1,14 +1,12 @@
 module Pagination
-    def resources_with_pagination(resources)
+    def pagination(resources)
         {
-        pagenation: {
             current:  resources.current_page,
             previous: resources.prev_page,
             next:     resources.next_page,   
             limit_value: resources.limit_value,
             pages:    resources.total_pages,
             count:    resources.total_count
-        }
         }
     end
 end
